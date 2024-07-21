8c58ab742ac51717a187ecfad944129ff92a6345
Exousia KASEKA <kasekaexousia@gmail.com>
2024-07-21 16:46:35 +0100

    validate user register and send data to the database

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,3 +1,7 @@
+import User from '#models/user'
+import app from '@adonisjs/core/services/app'
+import { registerUserValidator } from '#validators/auth'
+import { cuid } from '@adonisjs/core/helpers'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthController {
@@ -5,7 +9,16 @@ export default class AuthController {
     return view.render('pages/auth/register')
   }
   async handleRegister({ request }: HttpContext) {
-    return request.file('thumbnail')
+    const { username, email, password, thumbnail } =
+      await request.validateUsing(registerUserValidator)
+    if (!thumbnail) {
+    } else {
+      await thumbnail.move(app.makePath('public/users'), { name: `${cuid()}.${thumbnail.extname}` })
+    }
+
+    const filePath = `${thumbnail?.fileName}`
+    await User.create({ username, email, password, thumbnail: filePath })
+    return
   }
   login({ view }: HttpContext) {
     return view.render('pages/auth/login')
--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -1,1 +1,24 @@
 import vine from '@vinejs/vine'
+
+export const registerUserValidator = vine.compile(
+  vine.object({
+    username: vine
+      .string()
+      .trim()
+      .minLength(4)
+      .alphaNumeric()
+      .unique(async (db, value) => {
+        const users = await db.from('users').where('username', value).first()
+        return !users
+      }),
+    email: vine
+      .string()
+      .email()
+      .unique(async (db, value) => {
+        const mails = await db.from('users').where('email', value).first()
+        return !mails
+      }),
+    password: vine.string().trim().minLength(8),
+    thumbnail: vine.file({ extnames: ['jpg', 'png'], size: '10mb' }).optional(),
+  })
+)
