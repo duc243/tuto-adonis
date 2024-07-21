4626f6bc605fd8941538db489da40bde4303033c
Exousia KASEKA <kasekaexousia@gmail.com>
2024-07-21 04:24:35 +0100

    define route to sign-up or sign-in

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,10 @@
 |
 */
 
+import AuthController from '#controllers/auth_controller'
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/register', [AuthController, 'register']).as('auth.register')
+router.get('/login', [AuthController, 'login']).as('auth.login')
